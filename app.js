0d1b43e781422f19a075d0526da4f7711b0b463a
Marco Messmer <mhagspiel@gmail.com>
2018-05-04 08:14:50 +0200

    event tutorial

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Becoming a Vue ninja.',
-        name: 'people of the world',
-        url: 'http://www.youtube.com',
-        classes: ['one', 'two']
+        title: 'Becoming a Vue ninja.'
     },
     methods: {
-        greet(time){
-            return `Hello ${this.name} and good ${time} - ${this.title}`
-        }
+
     }
 })
